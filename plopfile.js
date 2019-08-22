c4379f817c31bcb2e192e2a0be5dd0e3f7387632
Vaibhav Shringarpure <vibs.sh@gmail.com>
2019-08-22 22:46:47 +0100

    Added options to choose between styles and components. Also added space between the hash and Author title

--- plopfile.js
+++ plopfile.js
@@ -1,72 +1,81 @@
 module.exports = plop => {
-  plop.setHelper('date', function() {
+  plop.setHelper("date", function() {
     let dateObj;
     const today = new Date();
     const date = today.getDate();
     const month = today.getMonth() + 1;
     const fullYear = today.getFullYear();
     const currentTime = today.toLocaleTimeString();
-    return dateObj = `${date}/${month}/${fullYear} ${currentTime}`;
+    return (dateObj = `${date}/${month}/${fullYear} ${currentTime}`);
   });
 
-  plop.setGenerator ('component', {
-    description: 'This is a ReactJS Component Scaffolding file',
+  plop.setGenerator("component", {
+    description: "This is a ReactJS Component Scaffolding file",
     prompts: [
       {
-        type: 'input',
-        name: 'ComponentName',
-        message: 'What is the name of your Component'
+        type: "input",
+        name: "ComponentName",
+        message: "What is the name of your Component"
       },
       {
-        type: 'list',
-        name: 'type',
-        choices: ['functional', 'class'],
-        message: 'Which Component do you want to create ?'
+        type: "list",
+        name: "type",
+        choices: ["functional", "class"],
+        message: "Which Component do you want to create ?"
       },
       {
-        type: 'input',
-        name: 'ComponentAuthor',
-        message: 'Who is the Author of this Component'
+        type: "list",
+        name: "folder",
+        choices: ["styles", "components"],
+        message: "Which folder does this component be in ?"
+      },
+      {
+        type: "input",
+        name: "ComponentAuthor",
+        message: "Who is the Author of this Component"
       }
     ],
     actions: data => {
+      debugger;
+      let folderPath = data.folder === "styles" ? "./src/components/styles/{{pascalCase ComponentName}}" : "./src/components/components/{{pascalCase ComponentName}}";
+
       let actions = [
         {
-          type: 'add',
-          path: './src/components/{{pascalCase ComponentName}}/{{pascalCase ComponentName}}.scss',
-          templateFile: './template/component.scss.hbs'
+          type: "add",
+          path: folderPath + "/{{pascalCase ComponentName}}.scss",
+          templateFile: "./template/component.scss.hbs"
         },
         {
-          type: 'add',
-          path: './src/components/{{pascalCase ComponentName}}/AUTHOR.md',
-          templateFile: './template/AUTHOR.hbs'
+          type: "add",
+          path: folderPath + "/AUTHOR.md",
+          templateFile: "./template/AUTHOR.hbs"
         },
         {
-          type: 'add',
-          path: './src/components/{{pascalCase ComponentName}}/data.yaml',
-          templateFile: './template/data.yaml.hbs'
+          type: "add",
+          path: folderPath + "/data.yaml",
+          templateFile: "./template/data.yaml.hbs"
         },
         {
-          type: 'add',
-          path: './src/components/{{pascalCase ComponentName}}/package.json',
-          templateFile:'./template/package.json.hbs'
+          type: "add",
+          path: folderPath + "/package.json",
+          templateFile: "./template/package.json.hbs"
         }
       ];
 
-      if(data.type === 'functional') {
+      if (data.type === "functional") {
         actions.push({
-          type: 'add',
-          path: './src/components/{{pascalCase ComponentName}}/{{pascalCase ComponentName}}.jsx',
-          templateFile: './template/component-function.hbs'
-        })
+          type: "add",
+          path: folderPath + "/{{pascalCase ComponentName}}.jsx",
+          templateFile: "./template/component-function.hbs"
+        });
       } else {
         actions.push({
-          type: 'add',
-          path: './src/components/{{pascalCase ComponentName}}/{{pascalCase ComponentName}}.jsx',
-          templateFile: './template/component-class.hbs'
-        })
+          type: "add",
+          path: folderPath + "/{{pascalCase ComponentName}}.jsx",
+          templateFile: "./template/component-class.hbs"
+        });
       }
-      return actions
-    },
-  })
-}
+      return actions;
+    }
+  });
+};
--- src/components/Button/AUTHOR.md
+++
@@ -1,3 +0,0 @@
-#Button Component
-## Author : VAIBHAV SHRINGARPURE
-### Created on: 22/8/2019 10:06:02 PM
